6bdfc932f1db2b9b95d5ecdf4b3890cec77fc91a
Daniela Damjanoska <daniela.damjanoska@intertec.io>
2022-12-04 08:50:37 +0100

    initial files created and initial packages installed

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { pink } from "@mui/material/colors";
+
+//pages imports
+import SignInPage from "./Pages/SignInPage";
+import SignUpPage from "./Pages/SignUpPage";
+import BlogPage from "./Pages/BlogPage";
+import NotFoundPage from "./Pages/NotFoundPage";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: pink[300],
+      main: pink[600],
+    },
+  },
+});
+
+const App: React.FC = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<SignInPage />}></Route>
+          <Route path="/SignUp" element={<SignUpPage />}></Route>
+          <Route path="/Blog" element={<BlogPage />}></Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
+};
+
+export default App;
